3bd0d626c0dbd075c96679f21a2be24a3e08999a
Tomaszunek <pawlowskitomaszu@gmail.com>
2018-12-09 23:37:20 +0100

    add search route

--- src/routes/_index.ts
+++ src/routes/_index.ts
@@ -5,6 +5,7 @@ import * as AppUserRoutes from './appusers'
 import * as ContentRoutes from './appContent'
 import * as MainPageContentRoutes from './appMainPageContent'
 import * as ProductsRoutes from './appProduct'
+import * as SearchContentRoutes from './search'
 
 export function initRoutes(app: Express) {
   winston.log('info', '--> Initialisations des routes')
@@ -18,6 +19,7 @@ export function initRoutes(app: Express) {
   ContentRoutes.routes(app)
   MainPageContentRoutes.routes(app)
   ProductsRoutes.routes(app)
+  SearchContentRoutes.routes(app)
 
   app.all('*', (req: Request, res: Response) => res.boom.notFound())
 }
